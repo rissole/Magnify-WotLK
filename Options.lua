27d8b358b4e9fc2936c162592ec45d9b17283ae1
OMGparticles <blacryu@gmail.com>
2025-09-01 17:03:28 -0700

    Zoom in/out speed is relative
    - Zoom steps are now relative instead of absolute, leading to a more consistent difference between zoom levels when zooming in/out.
    - Added options for max zoom level and zoom change per step.

--- Options.lua
+++ Options.lua
@@ -1,5 +1,19 @@
 local ADDON_NAME, Magnify = ...
 
+-- Constants
+Magnify.ENABLEPERSISTZOOM_DEFAULT = false
+Magnify.ENABLEOLDPARTYICONS_DEFAULT = false
+
+Magnify.MAXZOOM_DEFAULT = 4.0
+Magnify.MAXZOOM_SLIDER_MIN = 2.0
+Magnify.MAXZOOM_SLIDER_MAX = 10.0
+Magnify.MAXZOOM_SLIDER_STEP = 0.5
+
+Magnify.ZOOMSTEP_DEFAULT = 0.1
+Magnify.ZOOMSTEP_SLIDER_MIN = 0.01
+Magnify.ZOOMSTEP_SLIDER_MAX = 0.5
+Magnify.ZOOMSTEP_SLIDER_STEP = 0.01
+
 local panel = CreateFrame("Frame", nil, InterfaceOptionsFramePanelContainer)
 panel.name = ADDON_NAME
 InterfaceOptions_AddCategory(panel)
@@ -20,6 +34,24 @@ panel.EnableOldPartyIcons.tooltip = "Tick to disable the colored party icons on 
 _G[panel.EnableOldPartyIcons:GetName() .. "Text"]:SetText("Uncolored party icons");
 panel.EnableOldPartyIcons:SetPoint("TOPLEFT", panel.EnablePersistZoom, "BOTTOMLEFT", 0, 0)
 
+panel.MaxZoom = CreateFrame("Slider", "MagnifyOptionsMaxZoom", panel, 
+    "MagnifySliderTemplate");
+panel.MaxZoom:SetMinMaxValues(Magnify.MAXZOOM_SLIDER_MIN, Magnify.MAXZOOM_SLIDER_MAX);
+panel.MaxZoom:SetValueStep(Magnify.MAXZOOM_SLIDER_STEP);
+_G[panel.MaxZoom:GetName() .. "Text"]:SetText("Maximum Zoom");
+_G[panel.MaxZoom:GetName() .. "Low"]:SetText(Magnify.MAXZOOM_SLIDER_MIN);
+_G[panel.MaxZoom:GetName() .. "High"]:SetText(Magnify.MAXZOOM_SLIDER_MAX);
+panel.MaxZoom:SetPoint("TopLeft", panel.EnableOldPartyIcons, "BOTTOMLEFT", 0, -15);
+
+panel.ZoomStep = CreateFrame("Slider", "MagnifyOptionsZoomStep", panel, 
+    "MagnifySliderTemplate");
+panel.ZoomStep:SetMinMaxValues(Magnify.ZOOMSTEP_SLIDER_MIN, Magnify.ZOOMSTEP_SLIDER_MAX);
+panel.ZoomStep:SetValueStep(Magnify.ZOOMSTEP_SLIDER_STEP);
+_G[panel.ZoomStep:GetName() .. "Text"]:SetText("Zoom Speed");
+_G[panel.ZoomStep:GetName() .. "Low"]:SetText(Magnify.ZOOMSTEP_SLIDER_MIN);
+_G[panel.ZoomStep:GetName() .. "High"]:SetText(Magnify.ZOOMSTEP_SLIDER_MAX);
+panel.ZoomStep:SetPoint("TopLeft", panel.MaxZoom, "BOTTOMLEFT", 0, -30);
+
 function Magnify.InitOptions()
     panel.EnablePersistZoom:SetChecked(MagnifyOptions.enablePersistZoom)
     panel.EnablePersistZoom:SetScript("OnClick", function()
@@ -37,6 +69,20 @@ function Magnify.InitOptions()
         else
             MagnifyOptions.enableOldPartyIcons = false
         end
+    end)
+
+    panel.MaxZoom:SetValue(MagnifyOptions.maxZoom or Magnify.MAXZOOM_DEFAULT)
+    _G[panel.MaxZoom:GetName() .. "CurrentValueText"]:SetFormattedText("%.2f", panel.MaxZoom:GetValue())
+    panel.MaxZoom:SetScript("OnValueChanged", function()
+        MagnifyOptions.maxZoom = panel.MaxZoom:GetValue()
+        _G[panel.MaxZoom:GetName() .. "CurrentValueText"]:SetFormattedText("%.2f", panel.MaxZoom:GetValue())
+    end)
+
+    panel.ZoomStep:SetValue(MagnifyOptions.zoomStep or Magnify.ZOOMSTEP_DEFAULT)
+    _G[panel.ZoomStep:GetName() .. "CurrentValueText"]:SetFormattedText("%.2f", panel.ZoomStep:GetValue())
+    panel.ZoomStep:SetScript("OnValueChanged", function()
+        MagnifyOptions.zoomStep = panel.ZoomStep:GetValue()
+        _G[panel.ZoomStep:GetName() .. "CurrentValueText"]:SetFormattedText("%.2f", panel.ZoomStep:GetValue())
     end)
 end
 
